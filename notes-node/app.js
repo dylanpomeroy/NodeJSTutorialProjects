7a398b84180afddae39ca4d0bad5ba022d36fc9a
dylanpomeroy <dylan.pomeroy.j@gmail.com>
2019-02-04 15:58:20 -0500

    Added lodash practice + gitignore for node_modules dir

--- notes-node/app.js
+++ notes-node/app.js
@@ -2,10 +2,16 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log(`7 + -9 = ${notes.add(7, -9)}`);
+var filteredArray = _.uniq(['Dylan', 1, 'Dylan', 1, 2, 3, 4]);
+console.log(filteredArray);
 
+// console.log(_.isString(true));
+// console.log(_.isString('Dylan'));
+
+// console.log(`7 + -9 = ${notes.add(7, -9)}`);
 
 // var user = os.userInfo();
 
