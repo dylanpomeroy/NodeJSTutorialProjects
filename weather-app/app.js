d9bc65106be7b7072ba2457c1373c10ba853d878
dylanpomeroy <dylan.pomeroy.j@gmail.com>
2019-02-07 01:12:28 -0500

    setup yargs argument for dynamic address values in weather app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,28 @@
+
+const yargs = require('yargs')
 const request = require('request');
 
 const secretManager = require('./secrets.js');
 
 var secrets = secretManager.getSecrets();
 
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true,
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+var encodedAddress = encodeURIComponent(argv.address);
+
 request({
-    url: `https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20streed%20philadelphia&key=${secrets.googleApiGeolocationKey}`,
+    url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=${secrets.googleApiGeolocationKey}`,
     json: true,
 }, (error, response, body) => {
     console.log(`Address: ${body.results[0].formatted_address}`);
