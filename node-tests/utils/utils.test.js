47bcdc644e1aa88f9fdab9ca04ef470249f0a694
dylanpomeroy <dylan.pomeroy.j@gmail.com>
2019-02-09 13:43:49 -0500

    setup more test cases

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,52 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
     var res = utils.add(33, 11);
     
-    if (res != 44){
-        throw new Error(`Expected 44, but got ${res}`);
-    }
+    expect(res)
+        .toBeA('number')
+        .toBe(44);
 });
 
 it('should square a number', () => {
     var res = utils.square(3);
 
-    if (res !== 9){
-        throw new Error(`Expectd 9, but got ${res}`);
-    }
+    expect(res)
+        .toBeA('number')
+        .toBe(9);
 });
+
+it('should verify first and last names are set', () => {
+    var testUserObj = {};
+    var testName = 'Dylan Pomeroy'
+
+    var res = utils.setName(testUserObj, testName);
+
+    expect(res)
+        .toBeA('object')
+        .toInclude({
+            firstName: 'Dylan',
+            lastName: 'Pomeroy'
+        })
+})
+
+// it('should expect some values', () => {
+//     // expect(12)
+//     //     .toNotBe(12)
+
+//     // expect({name: 'Dylan'})
+//     //     .toEqual({name: 'Dylan'})
+
+//     // expect([2, 3, 4])
+//     //     .toExclude(1);
+
+//     // expect({
+//     //     name: 'Dylan',
+//     //     age: 23,
+//     //     location: 'Toronto'
+//     // }).toExclude({
+//     //     age: 25,
+//     // })
+// });
